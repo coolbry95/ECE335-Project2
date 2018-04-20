f09a48f24fd40945b1e89a2aafbff320d5d6545f
coolbry95 <coolbry95@gmail.com>
2018-04-20 14:50:55 -0400

    fixed up addPet

--- databasebuilder.h
+++ databasebuilder.h
@@ -12,8 +12,29 @@ private:
     PetDatabase pet_database;
     vector<Bundle> bundle_database;
 public:
-    addPet(string clas, string name, string type, double weight, double price, string specialAttribute);
+    addPet(string clas, string name, string type, double weight, double price, string specialAttribute) {
+        switch(clas) {
+        case "Dog":
+            addDog(name,type,weight,specialAttribute);
+        case "Cat":
+            if (specialAttribute == "true") {
+                addCat(name,type,weight,true);
+            } else {
+                addCat(name,type,weight,false);
+            }
+        case "Fish":
+            addFish(name,type,weight,specialAttribute);
+        case "Bird":
+            if (specialAttribute == "true") {
+                addBird(name,type,weight,true);
+            } else {
+                addBird(name,type,weight,false);
+            }
+        }
+    };
+
     addBundle(string name, double price, vector<string>);
+
     addDog(string name, string type, double weight, string category);
     addCat(string name, string type, double weight, bool fluffy);
     addFish(string name, string type, double weight, string waterType);
